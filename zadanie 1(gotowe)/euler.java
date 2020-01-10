afefdbe9dc0a593b5b0ef0b9f97aba5176f993d2
AdgarOlerve <piotrek.acocieto@gmail.com>
2020-01-10 16:06:02 +0100

    gotowe

--- zadanie 1(gotowe)/euler.java
+++ zadanie 1(gotowe)/euler.java
@@ -5,7 +5,7 @@ class euler
     {
         int a=0,b=1,c=0,suma=0,fib_max=4_000_000;
 
-        do
+        while(c<=fib_max)
         {
             c=a+b;
             if((c%2)==0)
@@ -15,9 +15,7 @@ class euler
             a=b;
             b=c;
         }
-        while(c<=fib_max);
-        System.out.println("Suma: "+suma);
-
+    System.out.println("Suma: "+suma);
     }
 
 }
---
+++ zadanie 3(gotowe)/euler.java
@@ -0,0 +1,63 @@
+
+public class euler
+{
+	public static void main(String args[])
+	{
+		int a=3,b=4,c=5,suma=1000;
+		boolean is_pit=false;
+		boolean is_suma_1k=false;
+		for(int i=3;i<=suma;i++)//iteruje a
+		{
+			for(int j=i+1;j<=suma;j++)//iteruje b
+			{
+				for(int k=j+1;k<=suma;k++)//iteruje c
+				{
+					is_suma_1k=false;//jesli obie te zmienne beda prawdziwe jednoczesnie
+					is_pit=false;	 //oznacza to,ze znaleziono szukana trojke pitagorejska
+					a=i;
+					b=j;
+					c=k;
+					is_suma_1k=check_sum(a,b,c,suma); //sprawdza czy suma a+b+c=1000
+					is_pit=check_pit(a,b,c);		  //sprawdza czy trojka jest pitagorejska
+					if((is_suma_1k==true) && (is_pit==true)) //sprawdzenie,czy oba warunki sa prawdziwe jednoczesnie
+					{
+						break;
+					}
+				}
+				if((is_suma_1k==true) && (is_pit==true))//sprawdzenie,czy oba warunki sa prawdziwe jednoczesnie
+					{
+						break;
+					}
+			}
+			if((is_suma_1k==true) && (is_pit==true))//sprawdzenie,czy oba warunki sa prawdziwe jednoczesnie
+					{
+						break;
+					}
+		}
+		System.out.println("a "+a+" "+"b "+b+" "+"c "+c);
+		System.out.println("a + b + c = "+(a+b+c));
+		System.out.println("a^2 + b^2 = "+((a*a)+(b*b))+" c^2 = "+(c*c));
+	}
+	public static boolean check_sum(int a,int b,int c,int suma) //sprawdza czy suma a+b+c to 1000
+	{
+		if(a+b+c==suma)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public static boolean check_pit(int a,int b,int c)//sprawdza,czy dane wartosci tworza trojke pitagorejska
+	{
+		if((a*a)+(b*b)==(c*c))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
